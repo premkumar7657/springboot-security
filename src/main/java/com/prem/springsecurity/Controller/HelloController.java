aff352f60f0fe1843f16373ac2179e3baaee8ffe
premkumar7657 <83229009+premkumar7657@users.noreply.github.com>
2025-09-12 00:41:56 +0000

    jdbc manager added successuflly

--- src/main/java/com/prem/springsecurity/Controller/HelloController.java
+++ src/main/java/com/prem/springsecurity/Controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.prem.springsecurity.Controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,10 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-
-    String hello()
+    public String hello()
     {
         return "hello";
+    }
+
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String helloAdmin()
+    {
+        return "hello admin";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
+    public String helloUser()
+    {
+        return "hello user";
     }
 
 }
--- src/main/java/com/prem/springsecurity/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/prem/springsecurity/SecurityConfig/SecurityConfig.java
@@ -1,29 +1,47 @@
 package com.prem.springsecurity.SecurityConfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
-        http.authorizeHttpRequests(authz -> authz.anyRequest().authenticated());
+        http.authorizeHttpRequests(authz -> authz.requestMatchers("/h2-console/**").permitAll()
+        .anyRequest().authenticated());
 
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // No sessions will be created or used by Spring Security
 
-        //http.formLogin(withDefaults());
+        // Disable CSRF for the H2 console
+          //http.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"));
+          http.csrf(csrf -> csrf.disable());
+      http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin())); // Allow frames from the same origin (for H2 console)
+
+
+        http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
@@ -32,19 +50,39 @@ public class SecurityConfig {
     @Bean
     UserDetailsService userDetailsService()
     {
+
        // return username -> org.springframework.security.core.userdetails.User
        UserDetails user1 = User.withUsername("user1")
-            .password("{noop}user1")
+            .password(passwordEncoder().encode("user1"))
             .roles("USER")
             .build();
 
             UserDetails user2 = User.withUsername("admin1")
-            .password("{noop}admin1")
+            .password(passwordEncoder().encode("admin1"))
             .roles("ADMIN")
             .build();
 
-            return new InMemoryUserDetailsManager(user1,user2);
+
+            JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+
+            jdbcUserDetailsManager.setDataSource(dataSource);
+
+            jdbcUserDetailsManager.createUser(user1);
+            jdbcUserDetailsManager.createUser(user2);
+
+           return jdbcUserDetailsManager;
+
+           // return new InMemoryUserDetailsManager(user1,user2);
+           //https://github.com/spring-projects/spring-security/blob/main/core/src/main/resources/org/springframework/security/core/userdetails/jdbc/users.ddl
     }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    
 
 
 }
